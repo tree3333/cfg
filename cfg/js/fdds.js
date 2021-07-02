b39de912e76d9a1cd0c932ed6b4d3443870ab05a
tree3 <31870495@qq.com>
2021-07-02 15:01:10 +0800

    update fdds, host, keywords

--- cfg/js/fdds.js
+++ cfg/js/fdds.js
@@ -5,14 +5,17 @@
  *     */                                                         
                                                                   
 //解锁樊登读书精品课程                                            
-//hosts = api.dushu.io,gate-api.dushu.io                          
+//hosts = https://gateway-api.dushu365.com
 var body = $response.body                                        
 
 //精品课程解锁                                                    
 body = body.replace(/"isBuyed":\w+/g,'"isBuyed":true')
+body = body.replace(/"isBought":\w+/g,'"isBought":true')
+body = body.replace(/"hasBought":\w+/g,'"hasBought":true')
 body = body.replace(/"buyed":\w+/g,'"buyed":true')
-//body = body.replace(/"trial":\w+/g,'"trial":true')
-//body = body.replace(/"free":\w+/g,'"free":true')
 body = body.replace(/"hasBuy":\d/g,'"hasBuy":1')
+body = body.replace(/"trial":\w+/g,'"trial":true')
+body = body.replace(/"free":\w+/g,'"free":true')
+body = body.replace(/"unlock":\w+/g,'"unlock":true')
 
 $done({body});      
