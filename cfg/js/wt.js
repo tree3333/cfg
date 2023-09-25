64566976a808a9baf2b10e4fc44d85a74cb300e0
tree3333 <31870495@qq.com>
2023-09-25 13:48:43 +0800

    update wt

--- cfg/js/wt.js
+++ cfg/js/wt.js
@@ -7,10 +7,12 @@
 var body = $response.body                                        
 
 //精品课程解锁                                                    
-body = body.replace(/"vip_trial_start_time":\d+/g,'"vip_trial_start_time":1688250476')
-body = body.replace(/"vip_show_expire_time":\d+/g,'"vip_show_expire_time":1791533676')
-body = body.replace(/"vip_current_type":\d+/g,'"vip_current_type":2')
-body = body.replace(/"vip_expire_time":\d+/g,'"vip_expire_time":1791533676')
-body = body.replace(/"vip_current_is_trial":\w+/g,'"vip_current_is_trial":true')
+body = body.replace(/"vip_show_expire_time":\s*\d+/g,'"vip_show_expire_time":2130534427')
+body = body.replace(/"vip_expire_time":\s*\d+/g,'"vip_expire_time":2130534427')
+body = body.replace(/"vip_current_type":\s*\d+/g,'"vip_current_type":1')
+body = body.replace(/"user_id":\s*\"\w+/g,'"user_id":\"77666677')
+//body = body.replace(/"vip_trial_start_time":\d+/g,'"vip_trial_start_time":1688250476')
+//body = body.replace(/"vip_current_type":\d+/g,'"vip_current_type":2')
+//body = body.replace(/"vip_current_is_trial":\w+/g,'"vip_current_is_trial":true')
 
 $done({body});      
